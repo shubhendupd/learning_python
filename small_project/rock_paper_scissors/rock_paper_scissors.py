7b2cddae205542ad982af33eaa400077a5ad085f
shubhendupd <shubhendu.prasad@live.in>
2026-08-21 16:52:29 +0530

    Completed rockpaperscissor

---
+++ small_project/rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,33 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+options = ["rock", "paper", "scissors"]
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        continue
+
+    computer_pick = options[random.randint(0, 2)]
+
+
+    print("Computer picked", computer_pick + ".")
+
+    if (
+        (user_input == "rock" and computer_pick == "scissors")
+        or (user_input == "paper" and computer_pick == "rock")
+        or (user_input == "scissors" and computer_pick == "paper")
+    ):
+        print("You won!")
+        user_wins += 1
+    elif user_input == computer_pick:
+        print("It's a tie!")
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print(f"You won {user_wins} times.")
+print(f"Computer won {computer_wins} times.")
