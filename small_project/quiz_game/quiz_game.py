456383227fbae2774959a8c4b522e58449c5c210
shubhendupd <shubhendu.prasad@live.in>
2026-08-21 14:35:24 +0530

    Completed quiz app

--- small_project/quiz_game/quiz_game.py
+++ small_project/quiz_game/quiz_game.py
@@ -1,6 +1,7 @@
 import sys
 
 print('Welcome to the Quiz Game!')
+score = 0
 
 playing = input('Do you want to play? (yes/no): ')
 if playing.lower() != 'yes':
@@ -12,5 +13,39 @@ print('Great! Let\'s start the quiz.')
 answer = input(" what does CPU stand for? ")
 if answer.lower() == 'central processing unit':
     print('Correct!')
+    score += 1
 else:
     print('Incorrect!')
+
+answer = input(" what does GPU stand for? ")
+if answer.lower() == 'graphics processing unit':
+    print('Correct!')
+    score += 1
+else:
+    print('Incorrect!')
+
+answer = input(" what does RAM stand for? ")
+if answer.lower() == 'random access memory':
+    print('Correct!')
+    score += 1
+else:
+    print('Incorrect!')
+
+answer = input(" what does psu stand for? ")
+if answer.lower() == 'power supply unit':
+    print('Correct!')
+    score += 1
+else:
+    print('Incorrect!')
+
+
+answer = input(" what does ups stand for? ")
+if answer.lower() == 'uninterruptible power supply':
+    print('Correct!')
+    score += 1
+else:
+    print('Incorrect!')
+
+print(' You got ' + str(score) + ' questions correct!')
+grade = (score / 5) * 100
+print('Your grade is: ' + str(grade) + '%')
