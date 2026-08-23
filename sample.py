e0d8236c944fc3ad5f664c940c4116f23d4bb469
shubhendupd <shubhendu.prasad@live.in>
2026-08-23 17:58:16 +0530

    Password manager v0 and v1 completed

--- sample.py
+++ sample.py
@@ -1,56 +1,18 @@
-import json
-import sys
-import os
-FILE = "data.json"
+#Sample
 
-def load_file():
-    if not os.path.exists(FILE):
-        return {}
-    try:
-        with open(FILE, "r") as f:
-            return json.load(f)
-    except json.JSONDecodeError:   # catches empty or corrupted file
-        return {}
+from cryptography.fernet import Fernet
 
-def save_file(python_obj):
-	with open(FILE, "w") as f:
-		json.dump(python_obj, f, indent = 2)
+#generate key
 
-def add_data(account, username, password):
-	python_obj = load_file()
-	python_obj[account] ={"username" : username , "password" :password}  # add new entry or update existing entry in python dict
-	save_file(python_obj)
+key = Fernet.generate_key() # base 64
+cipher = Fernet(key)
 
-def view_data(account):
-	x = load_file()
-	return x.get(account)
+#encrypt
 
+token = cipher.encrypt(b"m - hi sam")
+print(token)
 
+#decrypt
 
-
-
-
-
-while True:
-	print("Welcome to Password genrator! ")
-	option = input("Type A to store password  R for retrieve the password and Q to quit: ").lower()
-	if option == "q":
-		sys.exit()
-		
-	if option == "a":
-		account = input("Enter account: ")
-		username = input("Enter username: ")
-		password = input("Enter password: ")
-		add_data(account, username,password)
-		print("data saved ")
-	elif option == "r":
-		account = input("Enter username: ")
-		result = view_data(account)
-		if result is None:
-			print("Account not found!")
-		else:
-			print(f"{account} : {result}")
-	else:
-		print(" Please type correct option")
-			
-			
+decrypt = cipher.decrypt(token)
+print("decrypt")
